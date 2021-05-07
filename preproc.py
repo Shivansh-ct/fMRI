da1345726e59ba7cfea6d5aa35d9a3295cf866f9
Shivansh Chandra Tripathi <30630642+Shivansh-ct@users.noreply.github.com>
2021-05-08 05:20:58 +0530

    Add files via upload

--- preproc.py
+++ preproc.py
@@ -5,7 +5,7 @@ from scipy.fftpack import fft, ifft
 
 
 
-def temporal_filtering(data, TR, low, high):
+def temporal_filtering_operation(data, TR, low, high):
     temp_filtered = np.zeros(data.shape)
     if low>high:
         temp = high
@@ -13,25 +13,25 @@ def temporal_filtering(data, TR, low, high):
         low = temp
     high_pass = 1/low
     low_pass = 1/high
-    
-    freq = np.concatenate((np.linspace(0,1.0/(2.0*TR),data.shape[3]//2),np.linspace(-1.0/(2.0*TR),0,data.shape[3]//2)))
-    for x in range(data.shape[0]):
-        for y in range(data.shape[1]):
-            for z in range(data.shape[2]):
-                timeseries = data[x][y][z][:]
-                timeseries = fft(timeseries)
-                timeseries[np.abs(freq)>high_pass]=0
-                timeseries[np.abs(freq)<low_pass]=0
-                timeseries = ifft(timeseries)
-                temp_filtered[x][y][z][:] = timeseries
+    freq = np.fft.fftfreq(data.shape[3], d=1.0/(2.0*TR))
+    #freq = np.concatenate((np.linspace(0,1.0/(2.0*TR),data.shape[3]//2),np.linspace(-1.0/(2.0*TR),0,data.shape[3]//2)))
+    for dim_x in range(data.shape[0]):
+        for dim_y in range(data.shape[1]):
+            for dim_z in range(data.shape[2]):
+                single_voxel_timeseries = data[dim_x][dim_y][dim_z][:]
+                single_voxel_fft = fft(single_voxel_timeseries)
+                single_voxel_fft[np.abs(freq)>high_pass]=0
+                single_voxel_fft[np.abs(freq)<low_pass]=0
+                single_voxel_ifft = ifft(single_voxel_fft)
+                temp_filtered[dim_x][dim_y][dim_z][:] = single_voxel_ifft
     return temp_filtered
 
 
-
-def slice_time_correction(data, t, slicetimes, TR, f):
+#t is the Target Time
+def slice_time_correction_operation(data, t, slicetimes, TR, file_descriptor):
     c_img = np.zeros(data.shape)
     if t>TR or t<0:
-        f.write("SLICE TIME CORRECTION FAILURE")
+        file_descriptor.write("SLICE TIME CORRECTION FAILURE")
         return data
         
     else:
@@ -41,7 +41,7 @@ def slice_time_correction(data, t, slicetimes, TR, f):
                 for z in range(data.shape[2]):
                     
                         if slicetimes[z]>TR or slicetimes[z]<0:
-                            f.write("SLICE TIME CORRECTION FAILURE")
+                            file_descriptor.write("SLICE TIME CORRECTION FAILURE")
                             return data
                         
                         
@@ -53,8 +53,14 @@ def slice_time_correction(data, t, slicetimes, TR, f):
                         else:
                             alpha = np.array(((t - (slicetimes[z]*np.ones(data.shape[3])))/TR)[0:-1])
                             c_img[x][y][z][0:-1] =  np.add(np.multiply(alpha,data[x][y][z][1:]),np.multiply((1-alpha),data[x][y][z][0:-1]))                       
-                        c_img[x][y][z][data.shape[3]-1] = data[x][y][z][data.shape[3]-1] 
+                        c_img[x][y][z][data.shape[3]-1] = data[x][y][z][data.shape[3]-1]
+                        c_img[x][y][z][0] = data[x][y][z][0]
     #c_img[0:][0:][0:][data.shape[3]-1] = data[0:][:][:][data.shape[3]-1]
+    file_descriptor.write("SLICE TIME CORRECTION SUCCESS\n")
+    #c_img[0][:][:][:] = data[0][:][:][:]
+    #print(c_img.shape, data.shape, data.shape[3]-1)
+    #a = int(data.shape[3]-1)
+    #c_img[a][:][:][:] = data[a][:][:][:]
     return c_img
     
     
@@ -62,44 +68,45 @@ def slice_time_correction(data, t, slicetimes, TR, f):
     
 
     
-def spatial_smoothing(data, fwhm, kernel_size, voxel_size):
-    
-    smooth = np.zeros(data.shape)
-    for time in range(data.shape[3]):
-       #Applying along x direction
-       kernel_x = prep_kernel(fwhm, kernel_size, voxel_size[0])
-       for y in range(data.shape[1]):
-           for z in range(data.shape[2]):
-               smooth[:,y,z,time] = apply_kernel(data[:,y,z,time], kernel_x)
-       #Applying along y direction
-       kernel_y = prep_kernel(fwhm, kernel_size, voxel_size[1])
-       for x in range(data.shape[0]):
-           for z in range(data.shape[2]):
-               smooth[x,:,z,time] = apply_kernel(smooth[x,:,z,time], kernel_y)
-       #Applying along z direction
-       kernel_z = prep_kernel(fwhm, kernel_size, voxel_size[2])
-       for y in range(data.shape[1]):
-           for x in range(data.shape[0]):
-               smooth[x,y,:,time] = apply_kernel(smooth[x,y,:,time], kernel_z)
+def spatial_smoothing_operation(data, fwhm, voxel_size):
+    shape = data.shape
+    smooth = np.zeros(shape)
+    for time in range(0,data.shape[3]):
+       #Applying the operation along x direction
+       kernel_x = make_kernel_operation(fwhm, data.shape[0], voxel_size[0])
+       for dim_y in range(0,data.shape[1]):
+           for dim_z in range(0,data.shape[2]):
+               smooth[:,dim_y,dim_z,time] = convolving_kernel_operation(data[:,dim_y,dim_z,time], kernel_x)
+       #Applying the operation along y direction
+       kernel_y = make_kernel_operation(fwhm, data.shape[1], voxel_size[1])
+       for dim_x in range(0,data.shape[0]):
+           for dim_z in range(0,data.shape[2]):
+               smooth[dim_x,:,dim_z,time] = convolving_kernel_operation(smooth[dim_x,:,dim_z,time], kernel_y)
+       #Applying the operation along z direction
+       kernel_z = make_kernel_operation(fwhm, data.shape[2], voxel_size[2])
+       for dim_y in range(0,data.shape[1]):
+           for dim_x in range(0,data.shape[0]):
+               smooth[dim_x,dim_y,:,time] = convolving_kernel_operation(smooth[dim_x,dim_y,:,time], kernel_z)
+    #smooth represents the spatially smoothened image
     return smooth
     
 
 #Preparing a Gaussian Kernel
-def prep_kernel(fwhm, kernel_size, voxel_size):
-    v = np.arange(int(-1*kernel_size/2), int(kernel_size/2)+1)
+def make_kernel_operation(fwhm, kernel_size, voxel_size):
     sigma = fwhm/(np.sqrt(8*np.log(2))*voxel_size)
-    w = np.exp(-(v)**2/(2*(sigma**2)))
+    w = np.exp(-(np.arange(-kernel_size, kernel_size+1))**2/(2*(sigma**2)))
     kernel = w/sum(w)
     return kernel
-    
 
-def apply_kernel(series, kernel):
-    
+
+
+
+
+def convolving_kernel_operation(series, kernel):
     padded_series = np.concatenate((np.zeros(np.size(kernel)//2), series, np.zeros(np.size(kernel)//2)))
     conv_series = np.zeros(series.shape)
     for i in range(series.shape[0]):
         conv_series[i] = np.sum(np.multiply(padded_series[i:i+np.size(kernel)], kernel))
-    
     return conv_series
 
 
@@ -109,42 +116,49 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="input file", required=True)
     parser.add_argument("-o", "--output", help="output file", required=True)
-    parser.add_argument("-tc", "--slicetime", nargs=2, help="slice time acquisition")
-    parser.add_argument("-sm", "--fwhm", help="fwhm")
-    parser.add_argument("-tf", "--cutoff", nargs=2, help="temporal filtering")
-    parser.add_argument("-ts", "--timeseries", nargs=3, help="time series")
+    parser.add_argument("-tc", "--slicetime", nargs=2, help="target time and slice time acquisition file")
+    parser.add_argument("-tf", "--cutoff", nargs=2, help="temporal filtering high and low pass")
+    parser.add_argument("-sm", "--fwhm", help="spatial smoothing fwhm")
+    #parser.add_argument("-ts", "--timeseries", nargs=3, help="time series")
     args = parser.parse_args()
    # print(args.input, args.output, args.cutoff, args.slicetime, args.fwhm)
     
     
-    img = nb.load(args.input)
-    data = img.get_data()
-    print(args.cutoff, type(args.cutoff[0]), args.cutoff[1])
+    fmri_image = nb.load(args.input)
+    data = fmri_image.get_data()
+    #print(args.cutoff, type(args.cutoff[0]), args.cutoff[1])
     
-    header = img.header
+    header = fmri_image.header
     TR = header.get_zooms()[3]
     
     
-    voxel_size = header.get_zooms()[:3]
-    f = open(args.output + ".txt", 'a')
+    voxel_size_list = header.get_zooms()[:3]
+    #Opening the File in Write Mode
+    file_descriptor = open(args.output + ".txt", 'w')
     
     
     #print(data.shape, t, slicetimes.shape, TR, type(f))
     if args.slicetime!=None:
-        slicetimes = np.array(np.loadtxt(args.slicetime[1]), dtype=np.float32)
-        t = int(args.slicetime[0])
-        data = slice_time_correction(data, t, slicetimes, TR*1000, f)
-    if args.fwhm!=None:
-        data = spatial_smoothing(data, int(args.fwhm), 5, voxel_size)
+        slicetimes = np.loadtxt(args.slicetime[1])
+        t = float(args.slicetime[0])
+        data = slice_time_correction_operation(data, t, slicetimes, TR*1000, file_descriptor)
+        
     if args.cutoff!=None:
         low = float(args.cutoff[0])
         high = float(args.cutoff[1])        
-        data = temporal_filtering(data, TR, low, high)
+        data = temporal_filtering_operation(data, TR, low, high)
+        
+    if args.fwhm!=None:
+        data = spatial_smoothing_operation(data, int(args.fwhm),voxel_size_list)
     #print(fMRIData[5][5][5])
-    saveNII = True
-    outputImg = nb.Nifti1Image(data, np.eye(4), header=header)
-    outputNIIFileName = args.output + '.nii.gz'
-    nb.save(outputImg, outputNIIFileName)
-
+    
+    data = np.array(data, dtype=header.get_data_dtype())
+    Output_Array = nb.Nifti1Image(data, np.eye(4), header=header)
+    
+    Output_NIFTI_FileName = args.output + '.nii.gz'
+    
+    nb.save(Output_Array, Output_NIFTI_FileName)
+    #Closing the File before the program terminates
+    file_descriptor.close()
 
                 
